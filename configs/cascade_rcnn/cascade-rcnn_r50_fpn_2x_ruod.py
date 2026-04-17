017b64cd6a4a73166d177b636b9eb55149920060
BitStrawber <2837799357@qq.com>
2026-04-18 04:34:16 +0800

    fix: 重写data配置为train_dataloader格式

--- configs/cascade_rcnn/cascade-rcnn_r50_fpn_2x_ruod.py
+++ configs/cascade_rcnn/cascade-rcnn_r50_fpn_2x_ruod.py
@@ -14,7 +14,7 @@ classes = [
 ]
 num_classes = 10
 
-# Model: Cascade RCNN + ResNet-50 (ImageNet pretrained)
+# Model
 model = dict(
     init_cfg=dict(
         type='Pretrained',
@@ -62,6 +62,64 @@ model = dict(
                 loss_cls=dict(type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0),
                 loss_bbox=dict(type='SmoothL1Loss', beta=1.0, loss_weight=1.0))
         ])
+)
+
+# Data
+img_norm_cfg = dict(mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations', with_bbox=True),
+    dict(type='Resize', img_scale=(1920, 1080), keep_ratio=True),
+    dict(type='RandomFlip', flip_ratio=0.5),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='Pad', size_divisor=32),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels']),
+]
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='MultiScaleFlipAug', img_scale=(1920, 1080), flip=False,
+         transforms=[
+             dict(type='Resize', keep_ratio=True),
+             dict(type='Normalize', **img_norm_cfg),
+             dict(type='Pad', size_divisor=32),
+             dict(type='ImageToTensor', keys=['img']),
+             dict(type='Collect', keys=['img']),
+         ]),
+]
+data = dict(
+    samples_per_gpu=4,
+    workers_per_gpu=4,
+    train_dataloader=dict(
+        dataset=dict(
+            type='CocoDataset',
+            pipeline=train_pipeline,
+            ann_file=train_ann,
+            img_prefix=data_root + 'train/',
+            classes=classes),
+        sampler=dict(type='DefaultSampler', shuffle=True),
+        batch_sampler=dict(type='DefaultBatchSampler')),
+    val_dataloader=dict(
+        dataset=dict(
+            type='CocoDataset',
+            pipeline=test_pipeline,
+            ann_file=test_ann,
+            img_prefix=data_root + 'test/',
+            classes=classes)),
+    test_dataloader=dict(
+        dataset=dict(
+            type='CocoDataset',
+            pipeline=test_pipeline,
+            ann_file=test_ann,
+            img_prefix=data_root + 'test/',
+            classes=classes)))
+
+auto_scale_lr = dict(enable=False, base_batch_size=16)
+evaluation = dict(interval=1, save_best='auto', classwise=True)
+
+optim_wrapper = dict(
+    type='OptimWrapper',
+    optimizer=dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0001)
 )
 
 # Data: 8 GPUs, bs=2 per GPU (total BS=16), image size 1920x1080

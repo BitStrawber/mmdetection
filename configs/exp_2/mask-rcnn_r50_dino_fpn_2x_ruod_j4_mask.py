2698214c06e6487e215259a3e75821693d76b5ca
BitStrawber <2837799357@qq.com>
2026-04-27 01:15:20 +0800

    fix: simplify mask configs like official

--- configs/exp_2/mask-rcnn_r50_dino_fpn_2x_ruod_j4_mask.py
+++ configs/exp_2/mask-rcnn_r50_dino_fpn_2x_ruod_j4_mask.py
@@ -1,14 +1,13 @@
 # J4: ImageNet DINO + ResNet-50 + Mask R-CNN -> RUOD (10类)
 # 使用DINO预训练的ResNet-50权重
 
-_base_ = '../_base_/models/mask-rcnn_r50_fpn.py'
+_base_ = [
+    '../_base_/models/mask-rcnn_r50_fpn.py',
+    '../_base_/schedules/schedule_2x.py',
+    '../_base_/default_runtime.py',
+]
 
-# 导入mmdet
-custom_imports = dict(imports=['mmdet.models.detectors'])
-
-# 数据集路径配置 (绝对路径)
-data_root = '/media/HDD0/XCX/exp_2_data/exp_2/RUOD/coco/'
-ann_root = '/media/HDD0/XCX/exp_2_data/exp_2/RUOD/coco/annotations/'
+# 数据集路径配置
 data_root = '/media/HDD0/XCX/exp_2_data/exp_2/RUOD/coco/'
 ann_root = '/media/HDD0/XCX/exp_2_data/exp_2/RUOD/coco/annotations/'
 
@@ -20,23 +19,6 @@ model = dict(
         bbox_head=dict(num_classes=10),
         mask_head=dict(num_classes=10)))
 
-# 训练配置
-train_cfg = dict(type='EpochBasedTrainLoop', max_epochs=24, val_interval=1)
-val_cfg = dict(type='ValLoop')
-test_cfg = dict(type='TestLoop')
-
-optim_wrapper = dict(
-    optimizer=dict(type='SGD', lr=0.015, momentum=0.9, weight_decay=0.0001))
-
-param_scheduler = dict(
-    type='MultiStepLR',
-    begin=0,
-    end=24,
-    by_epoch=True,
-    milestones=[16, 22],
-    gamma=0.1)
-
-# 2 GPU配置 (总BS=12)
 train_dataloader = dict(
     batch_size=6, 
     num_workers=2,
@@ -44,6 +26,7 @@ train_dataloader = dict(
         data_root=data_root,
         data_prefix=dict(img='train/'),
         ann_file=ann_root + 'instances_train.json'))
+
 val_dataloader = dict(
     batch_size=1, 
     num_workers=2,
@@ -51,9 +34,8 @@ val_dataloader = dict(
         data_root=data_root,
         data_prefix=dict(img='val/'),
         ann_file=ann_root + 'instances_val.json'))
+
 test_dataloader = val_dataloader
 
-val_evaluator = dict(
-    ann_file=ann_root + 'instances_val.json',
-    metric=['bbox', 'segm'])
+val_evaluator = dict(ann_file=ann_root + 'instances_val.json')
 test_evaluator = val_evaluator

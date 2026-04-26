98af5613a2ef8ed2c522331581fbbab57304e3f1
BitStrawber <2837799357@qq.com>
2026-04-27 01:24:27 +0800

    add complete mask-rcnn ruod baseline config and simplify j2/j4 mask configs

--- configs/exp_2/mask-rcnn_r50_fpn_2x_ruod_j2_mask.py
+++ configs/exp_2/mask-rcnn_r50_fpn_2x_ruod_j2_mask.py
@@ -1,42 +1,13 @@
 # J2: ImageNet Supervised + ResNet-50 + Mask R-CNN -> RUOD (10类)
 # 使用torchvision内置的ResNet-50监督预训练权重
 
-_base_ = [
-    '../_base_/models/mask-rcnn_r50_fpn.py',
-    '../_base_/schedules/schedule_2x.py',
-    '../_base_/default_runtime.py',
-]
+_base_ = '../mask_rcnn/mask-rcnn_r50_fpn_2x_ruod.py'
 
-# 数据集路径配置
-data_root = '/media/HDD0/XCX/exp_2_data/exp_2/RUOD/coco/'
-ann_root = '/media/HDD0/XCX/exp_2_data/exp_2/RUOD/coco/annotations/'
-
-# 修改num_classes
-model = dict(
-    roi_head=dict(
-        bbox_head=dict(num_classes=10),
-        mask_head=dict(num_classes=10)))
-
+# 2 GPU配置 (总BS=12)
 train_dataloader = dict(
     batch_size=6, 
-    num_workers=2,
-    dataset=dict(
-        type='CocoDataset',
-        data_root=data_root,
-        data_prefix=dict(img='train/'),
-        ann_file=ann_root + 'instances_train.json'))
-
+    num_workers=2)
 val_dataloader = dict(
     batch_size=1, 
-    num_workers=2,
-    dataset=dict(
-        type='CocoDataset',
-        data_root=data_root,
-        data_prefix=dict(img='val/'),
-        ann_file=ann_root + 'instances_val.json',
-        test_mode=True))
-
+    num_workers=2)
 test_dataloader = val_dataloader
-
-val_evaluator = dict(type='CocoMetric', ann_file=ann_root + 'instances_val.json')
-test_evaluator = val_evaluator

e7be23a0eb885cc6ec3716aae6745729940983a7
BitStrawber <2837799357@qq.com>
2026-05-10 18:43:13 +0800

    update paths and change to 48 epochs

--- configs/exp_2/cross/faster-rcnn_r50_fpn_2x_cross.py
+++ configs/exp_2/cross/faster-rcnn_r50_fpn_2x_cross.py
@@ -1,12 +1,9 @@
-# Faster R-CNN: 筛选后的RUOD + DFUI 训练
+# Faster R-CNN: 筛选后的RUOD训练 (48 epoch)
 _base_ = '../../faster_rcnn/faster-rcnn_r50_fpn_2x_coco.py'
 
-# RUOD 筛选后数据集
-ruod_root = '/media/HDD0/XCX/exp_2_data/exp_2/RUOD/coco/'
-cross_dir = ruod_root + 'cross_split/'
-
-# DFUI 数据集
-dfui_root = '/media/HDD0/XCX/exp_2_data/dfui/'
+# RUOD 数据集路径
+ruod_root = '/media/HDD0/XCX/exp_2/RUOD/coco/'
+ann_root = ruod_root + 'annotations/'
 
 # 10类
 classes = ('holothurian', 'echinus', 'scallop', 'starfish', 'fish',
@@ -22,7 +19,7 @@ train_dataloader = dict(
     dataset=dict(
         data_root=ruod_root,
         data_prefix=dict(img='train/'),
-        ann_file=cross_dir + 'easy_merged.json',
+        ann_file=ann_root + 'easy_merged.json',
         metainfo=dict(classes=classes),
         filter_cfg=dict(filter_empty_gt=True, min_size=32)))
 
@@ -30,15 +27,16 @@ val_dataloader = dict(
     batch_size=1,
     num_workers=2,
     dataset=dict(
-        type='CocoDataset',
         data_root=ruod_root,
         data_prefix=dict(img='train/'),
-        ann_file=cross_dir + 'easy_merged.json',
+        ann_file=ann_root + 'easy_merged.json',
         metainfo=dict(classes=classes),
         test_mode=True))
 
 val_evaluator = dict(
-    ann_file=cross_dir + 'easy_merged.json',
+    ann_file=ann_root + 'easy_merged.json',
     metric='bbox')
 test_dataloader = val_dataloader
 test_evaluator = val_evaluator
+
+train_cfg = dict(type='EpochBasedTrainLoop', max_epochs=48, val_interval=1)

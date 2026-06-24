21de6c28db76373333976714332db8c21a3e1170
谢晨希 <2837799357@qq.com>
2026-06-24 20:41:09 +0800

    Fix J7 RUOD data path

--- configs/exp_2/cascade-rcnn_r50_dino-realuw_fpn_2x_ruod_j7.py
+++ configs/exp_2/cascade-rcnn_r50_dino-realuw_fpn_2x_ruod_j7.py
@@ -4,9 +4,27 @@ _base_ = './cascade-rcnn_r50_fpn_2x_ruod_j2.py'
 
 pretrained = '../pretrained_weights/j7_realuw_dino_resnet50_backbone.pth'
 
+data_root = '/media/HDD0/XCX/exp_2_data/exp_2/RUOD/coco/'
+ann_root = data_root + 'annotations/'
+
 model = dict(
     backbone=dict(
         init_cfg=dict(type='Pretrained', checkpoint=pretrained)))
+
+train_dataloader = dict(
+    dataset=dict(
+        data_root=data_root,
+        data_prefix=dict(img='train/'),
+        ann_file=ann_root + 'instances_train.json'))
+val_dataloader = dict(
+    dataset=dict(
+        data_root=data_root,
+        data_prefix=dict(img='val/'),
+        ann_file=ann_root + 'instances_val.json'))
+test_dataloader = val_dataloader
+
+val_evaluator = dict(ann_file=ann_root + 'instances_val.json')
+test_evaluator = val_evaluator
 
 load_from = None
 resume = False

e19adaef58c38d39f9b7cbc6d518a0463a2dc26e
BitStrawber <148919588+BitStrawber@users.noreply.github.com>
2026-09-12 20:39:10 +0800

    Add explicit nested DINO architecture recipes

--- configs/exp_2/tri_pretrain/s1_imagenet_dino_vits_100e.sh
+++ configs/exp_2/tri_pretrain/s1_imagenet_dino_vits_100e.sh
@@ -1,19 +1,16 @@
-# ImageNet-1K + facebookresearch/DINO + ViT-Small/16, 100 epochs.
-#
-# This mirrors the J14 RealUW ViT-S DINO recipe and only changes the output
-# experiment name. The data path is supplied by REALUW_SSL_ROOT/imagefolder/train
-# through run_exp_2_tri_pretrain_s1.sh.
+#!/usr/bin/env bash
+# Official Facebook DINO 100-epoch protocol used for the ImageNet, RealUW,
+# Synthetic5 and controlled-100K ViT-S/16 experiments.
 
-export EXP_ID="${EXP_ID:-j14}"
-export DINO_NAME="${DINO_NAME:-imagenet_dino_vits_100e}"
-export DINO_ARCH="${DINO_ARCH:-vit_small}"
-export DINO_OPTIMIZER="${DINO_OPTIMIZER:-adamw}"
-export DINO_LR="${DINO_LR:-0.0005}"
-export DINO_WEIGHT_DECAY="${DINO_WEIGHT_DECAY:-0.04}"
-export DINO_WEIGHT_DECAY_END="${DINO_WEIGHT_DECAY_END:-0.4}"
-export DINO_GLOBAL_CROPS_SCALE="${DINO_GLOBAL_CROPS_SCALE:-0.4 1}"
-export DINO_LOCAL_CROPS_SCALE="${DINO_LOCAL_CROPS_SCALE:-0.05 0.4}"
-export DINO_EPOCHS="${DINO_EPOCHS:-100}"
-export DINO_BATCH_SIZE_PER_GPU="${DINO_BATCH_SIZE_PER_GPU:-64}"
-export DINO_NUM_WORKERS="${DINO_NUM_WORKERS:-10}"
-export DINO_SAVECKP_FREQ="${DINO_SAVECKP_FREQ:-50}"
+export DINO_ARCH=vit_small
+export DINO_OPTIMIZER=adamw
+export DINO_LR=0.0005
+export DINO_WEIGHT_DECAY=0.04
+export DINO_WEIGHT_DECAY_END=0.4
+export DINO_WARMUP_EPOCHS=10
+export DINO_MIN_LR=1e-6
+export DINO_MOMENTUM_TEACHER=0.996
+export DINO_FREEZE_LAST_LAYER=1
+export DINO_GLOBAL_CROPS_SCALE='0.14 1'
+export DINO_LOCAL_CROPS_NUMBER=8
+export DINO_LOCAL_CROPS_SCALE='0.05 0.14'
